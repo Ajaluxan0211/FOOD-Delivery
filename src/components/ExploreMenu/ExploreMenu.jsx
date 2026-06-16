e504359665cf7e293ffa395175278fe45e2a2608
ajaluxan.t@gmail.com <ajaluxan.t@gmail.com>
2026-06-16 16:58:22 +0530

    update the ExploreMenu

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { menu_list } from '../../assets/assets'
 import './ExploreMenu.css'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h2>Explore our menu</h2>
@@ -16,8 +16,8 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt={item.menu_name} />
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All" : item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={category===item.menu_name? "active":""} src={item.menu_image} alt={item.menu_name} />
               <p>{item.menu_name}</p>
             </div>
           )
